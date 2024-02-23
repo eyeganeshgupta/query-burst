5e55a2ca4c02195ff8715ebd4e7a0975f313d329
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-02-23 11:40:51 +0530

    👉🏽rightsidebar-component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
@@ -16,6 +17,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         </section>
 
         {/* RightSideBar */}
+        <RightSidebar />
       </div>
       {/* Toaster Component */}
     </main>
