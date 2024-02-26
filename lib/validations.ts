b9d52d207ebde1c3f39caa4737d561e8cc25a525
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-02-26 19:36:44 +0530

    🏷️custom-multiple-tags-input⌨️

--- lib/validations.ts
+++ lib/validations.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const QuestionsSchema = z.object({
   title: z.string().min(5).max(150),
   explanation: z.string().min(20),
-  tags: z.array(z.string().min(1).max(30)).min(1).max(5),
+  tags: z.array(z.string().min(1).max(30)).min(1).max(3),
 });
