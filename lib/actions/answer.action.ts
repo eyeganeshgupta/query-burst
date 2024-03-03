7703282bcb96a33bdc5160fdfb4f9d2622eceacb
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-03 08:22:36 +0530

    🖥️display-all-answers📝

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -4,14 +4,14 @@ import Answer from "@/database/answer.model";
 import Question from "@/database/question.model";
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../mongoose";
-import { CreateAnswerParams } from "./shared.types";
+import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
 
 export async function createAnswer(params: CreateAnswerParams) {
   try {
     connectToDatabase();
     const { content, author, question, path } = params;
 
-    const newAnswer = new Answer({
+    const newAnswer = await Answer.create({
       content,
       author,
       question,
@@ -27,6 +27,20 @@ export async function createAnswer(params: CreateAnswerParams) {
     // TODO: add interaction...
 
     revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAnswers(params: GetAnswersParams) {
+  try {
+    connectToDatabase();
+    const { questionId } = params;
+    const answers = await Answer.find({ question: questionId })
+      .populate("author", "_id clerkId name picture")
+      .sort({ createdAt: -1 });
+    return { answers };
   } catch (error) {
     console.log(error);
     throw error;
