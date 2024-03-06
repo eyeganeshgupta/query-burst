d47e66f1abad9d66616b7f52952ffa1a6b91f72c
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-06 08:42:55 +0530

    🔍search-functionality-for-community-page👨‍👨‍👦‍👦

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -39,7 +39,25 @@ export async function getAllUsers(params: GetAllUsersParams) {
     connectToDatabase();
     // eslint-disable-next-line no-unused-vars
     const { page = 1, pageSize = 20, filter, searchQuery } = params;
-    const users = await User.find({}).sort({ createdAt: -1 });
+
+    const query: FilterQuery<typeof User> = {};
+
+    if (searchQuery) {
+      query.$or = [
+        {
+          name: {
+            $regex: new RegExp(searchQuery, "i"),
+          },
+        },
+        {
+          username: {
+            $regex: new RegExp(searchQuery, "i"),
+          },
+        },
+      ];
+    }
+
+    const users = await User.find(query).sort({ createdAt: -1 });
     return { users };
   } catch (error) {
     console.log(error);
