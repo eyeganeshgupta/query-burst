9d2403f5ab8004aaaab4909c8f426865c260fe10
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-08 09:08:41 +0530

    🗺️🕵🏻global-search-filters

--- components/shared/search/GlobalFilter.tsx
+++ components/shared/search/GlobalFilter.tsx
@@ -1,5 +1,61 @@
+"use client";
+
+import { GlobalSearchFilters } from "@/constants/filters";
+import { formUrlQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
 const GlobalFilter = () => {
-  return <div>GlobalFilter</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const typeParams = searchParams.get("type");
+
+  const [active, setActive] = useState(typeParams || "");
+
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "type",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "type",
+        value: item.toLowerCase(),
+      });
+
+      console.log(newUrl);
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-5 px-5">
+      <p className="text-dark400_light900 body-medium">Type: </p>
+      <div className="flex gap-3">
+        {GlobalSearchFilters.map((item) => {
+          return (
+            <button
+              key={item.value}
+              type="button"
+              className={`light-border-2 small-medium dark:text-light-800 rounded-2xl px-5 py-2 capitalize dark:hover:text-primary-500 ${active === item.value ? "bg-primary-500 text-light-900" : "bg-light-700 text-dark-400 hover:text-primary-500 dark:bg-dark-500"}`}
+              onClick={() => handleTypeClick(item.value)}
+            >
+              {item.value}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default GlobalFilter;
