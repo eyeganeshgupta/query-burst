c8e1c249ba40596e0655684e2bea3b0256a14afc
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-03-09 09:07:40 +0530

    display-the-🇦🇮-results-on-the-ui🖥️📱

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -82,10 +82,16 @@ const Answer = ({ question, questionId, authorId }: Props) => {
 
       const aiAnswer = await response.json();
 
-      alert(aiAnswer.reply);
+      // TODO: convert plain text to HTML format
+      const formattedAnswer = aiAnswer.reply.replace(/\n/g, "<br />");
+      if (editorRef.current) {
+        const editor = editorRef.current as any;
+        editor.setContent(formattedAnswer);
+      }
+
+      // TODO: toast notification
     } catch (error) {
       console.log(error);
-      throw error;
     } finally {
       setIsSubmittingAI(false);
     }
@@ -101,14 +107,20 @@ const Answer = ({ question, questionId, authorId }: Props) => {
           className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
           onClick={generateAiAnswer}
         >
-          <Image
-            src="/assets/icons/stars.svg"
-            alt="star"
-            width={12}
-            height={12}
-            className="object-contain"
-          />
-          Generate AI Answer
+          {isSubmittingAI ? (
+            <>Generating...</>
+          ) : (
+            <>
+              <Image
+                src="/assets/icons/stars.svg"
+                alt="star"
+                width={12}
+                height={12}
+                className="object-contain"
+              />
+              Generate AI Answer
+            </>
+          )}
         </Button>
       </div>
       <Form {...form}>
