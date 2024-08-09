a46a22701bea91e1d3ba198ccb6ea016a2440f03
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-08-09 10:17:54 +0530

    server-side-exception-fixed

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -11,6 +11,7 @@ import { auth } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Question Detail | Query Burst",
@@ -27,6 +28,8 @@ const QuestionDetailPage = async ({ params, searchParams }: any) => {
   if (clerkId) {
     mongoUser = await getUserById({ userId: clerkId });
     console.log(mongoUser);
+  } else {
+    redirect("/sign-in");
   }
 
   const result = await getQuestionById({ questionId: params.id });
